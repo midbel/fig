b59c7d64f867964d3ecf17c000badc00a31320c4
midbel <brun.nicolas21@gmail.com>
2021-05-23 21:01:57 +0200

    rewrite Token.String

--- token.go
+++ token.go
@@ -83,6 +83,76 @@ const (
 	Decrement
 )
 
+var types = map[rune]string{
+	EOF:          "eof",
+	Keyword:      "keyword",
+	Ident:        "ident",
+	Comment:      "comment",
+	Macro:        "macro",
+	Heredoc:      "heredoc",
+	String:       "string",
+	Integer:      "integer",
+	Float:        "float",
+	Date:         "date",
+	DateTime:     "datetime",
+	Time:         "time",
+	Boolean:      "boolean",
+	BegArr:       "beg-arr",
+	EndArr:       "end-arr",
+	BegObj:       "beg-obj",
+	EndObj:       "end-obj",
+	Comma:        "comma",
+	Semicolon:    "semicolon",
+	Question:     "question",
+	EOL:          "eol",
+	Not:          "not",
+	Assign:       "assign",
+	Add:          "add",
+	AddAssign:    "add-assign",
+	Sub:          "sub",
+	SubAssign:    "sub-assign",
+	Mul:          "mul",
+	MulAssign:    "mul-assign",
+	Div:          "div",
+	DivAssign:    "div-assign",
+	Mod:          "mod",
+	ModAssign:    "mod-assign",
+	Pow:          "pow",
+	Gt:           "gt",
+	Lt:           "lt",
+	Ge:           "ge",
+	Le:           "le",
+	Lshift:       "left-shift",
+	LshiftAssign: "left-shift-assign",
+	Rshift:       "right-shift",
+	RshiftAssign: "right-shift-assign",
+	Band:         "bin-and",
+	BandAssign:   "bin-and-assign",
+	Bor:          "bin-or",
+	BorAssign:    "bin-or-assign",
+	Bnot:         "bin-not",
+	And:          "and",
+	Or:           "or",
+	Equal:        "eq",
+	NotEqual:     "ne",
+	BegGrp:       "beg-grp",
+	EndGrp:       "end-grp",
+	LocalVar:     "local-var",
+	EnvVar:       "env-bar",
+	Invalid:      "invalid",
+	Let:          "let",
+	Ret:          "return",
+	If:           "if",
+	For:          "for",
+	While:        "while",
+	Else:         "else",
+	Foreach:      "foreach",
+	Break:        "break",
+	Continue:     "continue",
+	Increment:    "increment",
+	Decrement:    "decrement",
+}
+
 var assignments = map[rune]rune{
 	Assign:       Assign,
 	AddAssign:    Add,
@@ -149,138 +219,12 @@ func (t Token) exprDone() bool {
 }
 
 func (t Token) String() string {
-	var prefix string
-	switch t.Type {
-	case EOF:
-		return "<eof>"
-	case EOL:
-		return "<eol>"
-	case Ident:
-		prefix = "ident"
-	case Macro:
-		prefix = "macro"
-	case Comment:
-		prefix = "comment"
-	case Heredoc:
-		prefix = "heredoc"
-	case String:
-		prefix = "string"
-	case Integer:
-		prefix = "integer"
-	case Float:
-		prefix = "float"
-	case Date:
-		prefix = "date"
-	case DateTime:
-		prefix = "datetime"
-	case Time:
-		prefix = "time"
-	case Boolean:
-		prefix = "boolean"
-	case BegArr:
-		return "<beg-arr>"
-	case EndArr:
-		return "<end-arr>"
-	case BegGrp:
-		return "<beg-grp>"
-	case EndGrp:
-		return "<end-grp>"
-	case BegObj:
-		return "<beg-obj>"
-	case EndObj:
-		return "<end-obj>"
-	case Assign:
-		return "<assign>"
-	case Comma:
-		return "<comma>"
-	case Semicolon:
-		return "<Semicolon>"
-	case Question:
-		return "<question>"
-	case Invalid:
-		prefix = "invalid"
-	case Not:
-		return "<not>"
-	case Equal:
-		return "<eq>"
-	case NotEqual:
-		return "<ne>"
-	case Gt:
-		return "<gt>"
-	case Lt:
-		return "<lt>"
-	case Ge:
-		return "<ge>"
-	case Le:
-		return "<le>"
-	case Add:
-		return "<add>"
-	case AddAssign:
-		return "<add-assign>"
-	case Sub:
-		return "<subtract>"
-	case SubAssign:
-		return "<subtract-assign>"
-	case Div:
-		return "<divide>"
-	case DivAssign:
-		return "<divide-assign>"
-	case Mul:
-		return "<multiply>"
-	case MulAssign:
-		return "<multiply-assign>"
-	case Mod:
-		return "<modulo>"
-	case ModAssign:
-		return "<modulo-assign>"
-	case Pow:
-		return "<power>"
-	case And:
-		return "<and>"
-	case Or:
-		return "<or>"
-	case Band:
-		return "<bin-and>"
-	case BandAssign:
-		return "<bin-and-assign>"
-	case Bor:
-		return "<bin-or>"
-	case BorAssign:
-		return "<bin-or-assign>"
-	case Bnot:
-		return "<bin-not>"
-	case Lshift:
-		return "<left-shift>"
-	case LshiftAssign:
-		return "<left-shift-assign>"
-	case Rshift:
-		return "<right-shift>"
-	case RshiftAssign:
-		return "<right-shift-assign>"
-	case LocalVar:
-		prefix = "local"
-	case EnvVar:
-		prefix = "env"
-	case Keyword:
-		prefix = "keyword"
-	case Ret:
-		return "<return>"
-	case Let:
-		return "<let>"
-	case If:
-		return "<if>"
-	case For, While, Foreach:
-		prefix = "loop"
-	case Break:
-		return "<break>"
-	case Continue:
-		return "<continue>"
-	case Increment:
-		return "<increment>"
-	case Decrement:
-		return "<decrement>"
-	default:
-		prefix = "unknown"
+	prefix, ok := types[t.Type]
+	if !ok {
+		return "<unknown>"
+	}
+	if t.Input == "" {
+		return fmt.Sprintf("<%s>", prefix)
 	}
 	return fmt.Sprintf("<%s(%s)>", prefix, t.Input)
 }
